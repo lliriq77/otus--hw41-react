b167eb9895f9929b3db687a345861e5940ab0f0b
Kirill Tikhonov <lliriq@me.com>
2022-07-18 00:01:55 +0300

    GHP update

--- src/app.tsx
+++ src/app.tsx
@@ -4,26 +4,23 @@ import { Input } from "./components/Input/index";
 import { Header } from "./components/Header";
 
 export const App = () => {
-    const [show, setShow] = React.useState(true);
-    const toggleShow = () => {
-        setShow((prevShow) => !prevShow);
-    };
+  const [show, setShow] = React.useState(true);
+  const toggleShow = () => {
+    setShow((prevShow) => !prevShow);
+  };
 
-    return (
+  return (
+    <>
+      <Header label={"Hello2"} />
+      <Button label={show ? "close" : "open"} onButtonClick={toggleShow} />
+      <hr />
+      {show && (
         <>
-            <Header label={"Hello2"} />
-            <Button
-                label={show ? "close" : "open"}
-                onButtonClick={toggleShow}
-            />
-            <hr />
-            {show && (
-                <>
-                    <Button label={"cancel"} disabled />
-                    <Button label={"submit"} />
-                    <Input />
-                </>
-            )}
+          <Button label={"cancel"} disabled />
+          <Button label={"submit"} />
+          <Input />
         </>
-    );
+      )}
+    </>
+  );
 };
